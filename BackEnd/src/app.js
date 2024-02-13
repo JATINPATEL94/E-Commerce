400ce35bf4ba5eb53bd6a8cb9665b1851e190d06
JATINPATEL94 <pateljatinpc@gmail.com>
2024-02-13 22:56:53 +0530

    Allow All Origins CORS

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,34 +1,34 @@
-import express, { urlencoded } from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
+// CORS middleware
 app.use(
   cors({
-    origin: "https://styleblend.netlify.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    // origin: process.env.CORS_ORIGIN,
-    // credentials: true,
   })
 );
 
-// express configuration
+// Body parsers
 app.use(express.json({ limit: "15kb" }));
-
 app.use(express.urlencoded({ extended: true, limit: "15kb" }));
 
+// Static files
 app.use(express.static("public"));
 
+// Cookie parser
 app.use(cookieParser());
 
-// routes import
+// Import routes
 import userRouter from "./routes/user.routes.js";
 import productRouter from "./routes/product.routes.js";
 import paymentRouter from "./routes/payment.routes.js";
 import orderRouter from "./routes/order.routes.js";
 
-//routes declaration  (prefix routes)
+// Route declarations (prefix routes)
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/product", productRouter);
 app.use("/api/v1/payment", paymentRouter);
